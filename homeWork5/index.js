38d74d520cc260723d6e61b06d6214723fac8a96
YershovArt <artemyershov1990@gmail.com>
2023-03-20 19:16:02 +0200

    directory homeWork3 - index.html file was changed tittle was added,directory homeWork5 was added  - index.html file was added , index.js was added with code to solve tusk#5

---
+++ homeWork5/index.js
@@ -0,0 +1,22 @@
+// TODO Створити скрипт який має визначити середнє арифметичне трьох чисел з насутпними умовами:
+// отримати від користувача через три prompt три числа
+// показати через alert середнє арифметичне цих чисел
+while(true) {
+alert('COUNT AVERAGE OF 3 NUMBERS')
+let firstNumber = Number(prompt('Please, enter first number', ''));
+let secondNumber = Number(prompt('Please, enter second number', ''));
+let thirdNumber = Number(prompt('Please, enter third number', ''));
+if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber) || Number.isNaN(thirdNumber)) {
+    alert('Numbers only!')
+} else {
+    let sumOfNumbers = firstNumber+secondNumber+thirdNumber;
+    alert(`AVERAGE = ${sumOfNumbers/3}`);
+}
+}
+
+
+
+
+
+
+
