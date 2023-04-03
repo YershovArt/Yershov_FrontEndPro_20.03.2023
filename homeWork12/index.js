4abef47cc0736c748116c9d66de8834754c06a3b
YershovArt <artemyershov1990@gmail.com>
2023-04-03 16:16:05 +0300

    fixed homeWork12

--- homeWork12/index.js
+++ homeWork12/index.js
@@ -1,39 +1,18 @@
 // TODO Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 let arr = [1, 2, 3, 4, 5, 6, 7 ];
 
-// First way
-//function returnArr(array, toRemove) {
-//    for (let i = 0; i<array.length; i++) {
-//        if (array[i] === toRemove) {
-//        delete array[i];
-//        }
-//    }
-//    return array;
-//};
-//
-//console.log(returnArr(arr, 6));
+function returnArr(array, toRemove) {
 
-// Second way
-//function returnArr(array, toRemove) {
-//    let newArr = [];
-//    for (let i = 0; i<array.length; i++) {
-//        if (array[i] !== toRemove) {
-//            newArr.push(array[i]);
-//        }
-//    }
-//    return newArrx;
-//};
+    let newArr = [];
 
-//console.log(returnArr(arr, 6));
+    for (let i = 0; i<array.length; i++) {
+        if (array[i] !== toRemove) {
+            newArr.push(array[i]);
+        }
+    }
 
-// Third way
-//function returnArr(array, toRemove) {
-//    for (let i = 0; i<array.length; i++) {
-//        if (array[i] === toRemove) {
-//            array.splice(i, 1);
-//        }
-//    }
-//  return array;
-//};
-//
-//console.log(returnArr(arr, 6));
+    return newArr;
+
+};
+
+console.log(returnArr(arr, 6));
