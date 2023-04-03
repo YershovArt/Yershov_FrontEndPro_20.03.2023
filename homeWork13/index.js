cd4729ae9254df44544636f231a2f9354b0e4716
YershovArt <artemyershov1990@gmail.com>
2023-04-03 09:31:41 +0300

    homeWork13 some corrections was added

--- homeWork13/index.js
+++ homeWork13/index.js
@@ -1,8 +1,8 @@
 // TODO Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-function generateKey(length, char) {
+function generateKey(quantity, char) {
     let newString = ''
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < quantity; i++) {
         k = Math.floor(Math.floor(Math.random() * (char.length - 0) + 0));
         newString += char[k];
     }
