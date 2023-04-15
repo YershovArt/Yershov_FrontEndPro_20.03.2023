dda27c3ab70847987f768107017c7d1f705fccb3
YershovArt <artemyershov1990@gmail.com>
2023-04-15 10:45:18 +0300

    homeWork19 done

---
+++ homeWork19/index.js
@@ -0,0 +1,30 @@
+//TODO  Це ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
+//TODO Змініть код методів up, down і showStep таким Таким чином, щоб їх виклик можна було зробити по ланцюжку,
+//наприклад:ladder.up().up().down().showStep(); // 1
+let ladder = {
+
+    step: 0,
+
+    up: function () {
+        this.step++;
+
+        return this;
+
+    },
+    down: function () {
+        this.step--;
+
+        return this;
+
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+
+        return this;
+
+    }
+};
+
+// читав цей розділ, бачив цю задачу і рішення тут https://uk.javascript.info/object-methods
+// хочу зазначити, що після твого пояснення усе встало на місця , мені так здалося. Дякую!
+ladder.up().up().down().showStep(); // 1
