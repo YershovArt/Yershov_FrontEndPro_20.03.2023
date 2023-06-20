bd1c57709c8c4c869ea60177fe567b268202028b
YershovArt <artemyerhov1990@gmail.com>
2023-06-20 16:20:38 +0300

    homeWork36 done

---
+++ homeWork36/index.js
@@ -0,0 +1,16 @@
+//TODO Написати функцію generateList(array), яка приймає масив із чисел та масивів чисел (наприклад [1,2, 3])
+// і генерує список з елементів:
+
+let arr = [1, 2, 3, [2,3,[11,11,[22]]]];
+
+function generateList(array) {
+    let ul = document.querySelector('ul');
+    array.forEach(arrEl => {
+        let li = document.createElement('li');
+        Array.isArray(arrEl) ? li.appendChild(generateList(arrEl)) : li.innerHTML = `<li>${arrEl}</li>>`;
+        ul.appendChild(li)
+    });
+    return ul;
+}
+
+document.querySelector('body').appendChild(generateList(arr));
