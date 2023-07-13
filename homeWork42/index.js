0912f42a7bbadc2e168df9b3df4a53827edf784c
YershovArt <artemyershov1990@gmail.com>
2023-07-14 01:33:24 +0300

    homeWork42 done

--- homeWork42/index.js
+++ homeWork42/index.js
@@ -181,6 +181,11 @@ function infomationalWindow (orderDate, forObject, name, surname, phone, city, d
     return forObject;
 }
 
+// TODO Модифікувати інтернет-магазин таким чином, щоб була можливість переглянути всі збережені замовлення навіть після оновлення сторінки (використовувати localStorage).
+// На сторінці спочатку крім списку категорій відображається також кнопка “мої замовлення”.
+// При кліку на “мої замовлення” - пропадають категорії та відображається список усіх замовлень користувача (дата та ціна) - при кліку на замовлення - “розгортаються” деталі замовлення.
+// Реалізувати можливість видалення замовлення зі списку.
+
 function getDate() {
     let date = new Date();
     let getMonth = date.getMonth()+1;
@@ -213,8 +218,8 @@ function collectData() {
     menuContainer.insertAdjacentHTML("afterbegin", `<p class='menu-message__paragraph'>Than you for your Order ${clientOrder["Name"]}</p>`)
 };
 
-function saveToLocalStorage(name, object) {
-    localStorage.setItem(JSON.stringify(name), JSON.stringify(object));
+function saveToLocalStorage(key, data) {
+    localStorage.setItem(JSON.stringify(key), JSON.stringify(data));
 }
 
 myOrdersCategory.addEventListener('click', function () {
